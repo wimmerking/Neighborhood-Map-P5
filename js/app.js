63678a47f5218c9e1ce31a8bc27be922998a4025
Andrew King <wimmerking@gmail.com>
2016-03-11 22:43:11 -0800

    first commit with title and working map

---
+++ js/app.js
@@ -0,0 +1,8 @@
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 37.771799, lng: -122.176620},
+          zoom: 11
+        });
+      }
+      //37.771799, -122.176620
